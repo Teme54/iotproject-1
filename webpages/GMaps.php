5ee2011c4b2cf1e619656c1edb835774f9e8fa99
nks <niklas.poussu3@luukku.com>
2017-11-21 11:30:54 +0200

    More marker functionality and updating the map added

--- webpages/GMaps.php
+++ webpages/GMaps.php
@@ -3,30 +3,72 @@
     <meta name="viewport" content="initial-scale=1.0, user-scalable=no" />
     <meta http-equiv="content-type" content="text/html; charset=UTF-8"/>
     <title>Google Maps Geolocator 1.1a</title>
+
     <style>
 
        #map {
+         position: absolute;
+         display: block;
+         margin: auto;
          height: 100%;
          width: 100%;
-       }
 
+       }
+       #updateMapBtn {
+         height: 50px;
+         width: 100px;
+         z-index: 100;
+         position: absolute;
+         left: 100px;
+         top: 150px;
+         background-color: white;
+         font-weight: bold;
+       }
       html, body {
         height: 100%;
+
         margin: 0;
         padding: 0;
       }
-    </style>
-  </head>
 
+      #header {
+        z-index: 100;
+        position: absolute;
+        color: black;
+        font-size: 40px;
+        font-weight: bold;
+        left: 100px;
+        top: 50px;
+        border-style: solid;
+        padding: 8px 8px 8px 8px;
+        background-color: white;
+      }
+
+    </style>
+
+  </head>
+<h1 id=header>LocatIot Tracker v. 0.1a (EARLY ACCESS BUILD)</h1>
   <body>
+    <button type=button id=updateMapBtn onclick="updateMap()">Update Map</button>
+
     <div id="map"></div>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
 
     <script>
 
-        var timer = setInterval(updateMap, 10000);
+        var activeInfoWindow;
+
+        function updateMap () {
+          $.ajax({ url: "http://139.59.155.145/XML_SQL.php",
+              type: "GET",
+              url: 'http://139.59.155.145/XML_SQL.php',
+              data: {usernamelogin: 'stduser', passwordlogin: 'samplepass916'},
+              });
+        initMap();
+        }
 
         function initMap() {
+
         var map = new google.maps.Map(document.getElementById('map'), {
           center: new google.maps.LatLng(64.9973158, 25.4867483),
           zoom: 12
@@ -39,22 +81,31 @@
 
             Array.prototype.forEach.call(markers, function(markerElem) {
 
-              //var ID = markerElem.getAttribute('ID');
-              //var timestamp = markerElem.getAttribute('timestamp');
+              var ID = markerElem.getAttribute('ID');
+              var timestamp = markerElem.getAttribute('timestamp');
 
-              var point = new google.maps.LatLng(
-                  parseFloat(markerElem.getAttribute('latitude')),
-                  parseFloat(markerElem.getAttribute('longitude')));
+              var iLatitude = parseFloat(markerElem.getAttribute('latitude'));
+              var iLongitude = parseFloat(markerElem.getAttribute('longitude'));
+
+              var point = new google.maps.LatLng(iLatitude, iLongitude);
 
               var marker = new google.maps.Marker({
                 map: map,
                 position: point,
 
               });
+              marker['infowindow'] = new google.maps.InfoWindow({
+                content: "ID: " + ID + "<br>" + "Timestamp: " + timestamp + "<br>" + "Latitude: " + iLatitude + "<br>" + "Longitude: " + iLongitude
+              });
+
+              google.maps.event.addListener(marker, 'click', function() {
+                activeInfoWindow && activeInfoWindow.close();
+                this['infowindow'].open(map, this);
+                activeInfoWindow = this['infowindow'];
+              });
             });
           });
-        }
-
+      }
 
       function downloadUrl(url, callback) {
         var request = window.ActiveXObject ?
@@ -72,21 +123,10 @@
         request.send(null);
       }
 
-      function updateMap () {
-      $(document).ready(function(){
-        $.ajax({ url: "http://139.59.155.145/XML_SQL.php",
-            type: "GET",
-            url: 'http://139.59.155.145/XML_SQL.php',
-            data: {usernamelogin: 'stduser', passwordlogin: 'samplepass916'},
-            });
-      });
-      initMap();
-      }
-
       function doNothing() {}
     </script>
     <script async defer
-    src="https://maps.googleapis.com/maps/api/js?key=AIzaSyD3cAdJ4TwzleiMXiwKfDlwNeB2JLUmomY&callback=initMap">
+    src="https://maps.googleapis.com/maps/api/js?key=AIzaSyD3cAdJ4TwzleiMXiwKfDlwNeB2JLUmomY&callback=updateMap">
     </script>
   </body>
 </html>
